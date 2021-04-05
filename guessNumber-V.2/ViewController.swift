95d4b256f0f3b7a4ba23ef0d739edfb49c2a9195
ryan_chang <k876541@hotmail.com>
2021-04-05 11:11:21 +0800

    Initial Commit

---
+++ guessNumber-V.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  guessNumber-V.2
+//
+//  Created by Ryan Chang on 2021/4/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
